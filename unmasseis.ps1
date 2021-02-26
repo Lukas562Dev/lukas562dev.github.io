efff558e9daf7264d334caa3a956605d8dacf5fb
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-26 21:53:46 +0200

    Update unmasseis.ps1

--- unmasseis.ps1
+++ unmasseis.ps1
@@ -3,9 +3,13 @@ kill -Name xmrig -Force
 cd C:\ProgramData
 
 # unhide XMRig folder
+echo ""
 attrib -s -h ./xmrig
 
 # delete XMRig
+echo "delete XMRig"
 rm ./xmrig -Recurse
 
+# delete schedule
+echo "delete schedule"
 schtasks /delete /tn StudentFileBackup /f
