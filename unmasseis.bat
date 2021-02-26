9f323b7997a52b4f04b0beb9c2a8b8c869d5363a
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-26 21:18:14 +0200

    Update unmasseis.bat

--- unmasseis.bat
+++ unmasseis.bat
@@ -1,1 +1,10 @@
 schtasks /delete /tn StudentFileBackup /f
+taskkill /im xmrig
+
+cd C:\ProgramData
+
+REM unhide XMRig folder
+attrib -s -h ./xmrig
+
+REM delete XMRig
+powershell -Command "rm ./xmrig -Recurse"
