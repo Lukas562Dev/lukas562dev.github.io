2778842a05abc72a8ace997b09e78bfeb2784ee8
luukas <lukasdoesdev@gmail.com>
2021-03-23 18:12:03 +0200

    forgot to update config

--- src/blogconfig.py
+++ src/blogconfig.py
@@ -1,4 +1,4 @@
 logo = 'https://www.thatonelukas.tk/pictureLogo.png'
 website_domain = 'thatonelukas.tk'
 blog_location = f'https://www.thatonelukas.tk/blog'
-blog_location = f'http://127.0.0.1:5500/blog'
+# blog_location = f'http://127.0.0.1:5500/blog'
